33ebcf16922cab8ca081b6567fdee4b044edaafa
RobertoRugama <roberto.rugama01@gmail.com>
2022-03-23 11:26:02 -0600

    Initial commit

---
+++ src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServcioCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServcioCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServcioCommonsApplication.class, args);
+	}
+
+}
